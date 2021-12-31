b4f466b16741ec1fe47f76413f911571eb0fcec2
Aaron Flores Pasos <aaron.flores7908@alumnos.udg.mx>
2021-12-31 16:53:24 -0600

    Menú eliminar terminado

--- src/cmd/functions.py
+++ src/cmd/functions.py
@@ -13,6 +13,7 @@ def main():
 # < --------------- Funciones de menú y elegir opción "insertar" --------------- >
 
 def option_insert():
+    header_insert()
     title_insert()
     op_insert = int(input('>>> '))
     menu_insert(op_insert)
@@ -28,6 +29,7 @@ def menu_insert(op_insert):
         three_insert()
     
     elif op_insert == 0:
+        ChargingBarToClose()
         pass
 
     else:
@@ -53,6 +55,9 @@ def three_insert():
         makefile()
 
 def requirements():
+    connection = sqlite3.connect("src/database/database.db")
+    cursor = connection.cursor()
+
     print('INGRESA LA MATRICULA')
     enrollment = str(input('>>> '))
     print('INGRESA EL NOMBRE')
@@ -69,7 +74,7 @@ def requirements():
     connection.commit()
     connection.close()
 
-    tqdmProgressBar()
+    tqdmProgressBarSave()
 
 def makefile():
     print('INGRESA LA MATRICULA')
@@ -105,27 +110,29 @@ def makefile():
     connection.commit()
     connection.close()
 
-    tqdmProgressBar()
+    tqdmProgressBarSave()
     
     
 # < --------------- Funciones de menú y elegir opción "eliminar" --------------- >
 
 def option_delete():
+    header_delete()
     title_delete()
     op_delete = int(input('>>> '))
     menu_delete(op_delete)
 
 def menu_delete(op_delete):
     if op_delete == 1:
-        pass
+        one_delete()
 
     elif op_delete == 2:
-        pass
+        two_delete()
 
     elif op_delete == 3:
-        pass
+        three_delete()
 
     elif op_delete == 0:
+        ChargingBarToClose()
         pass
 
     else:
@@ -134,12 +141,40 @@ def menu_delete(op_delete):
 
 def one_delete():
     header_delete()
+    delete = "DELETE FROM records WHERE MATRICULA = ?"
+    print('INGRESA LA MATRICULA')
+    enrollment = str(input('>>> '))
+            
+    adr = (enrollment, )
+    cursor.execute(delete, adr)
+    cursor.close()
+    connection.commit()
+    connection.close()
+    tqdmProgressBarDelete()
 
 def two_delete():
     header_delete()
+    delete = "DELETE FROM records"
+    cursor.execute(delete)
+    cursor.close()
+    connection.commit()
+    connection.close()
+    tqdmProgressBarDelete()
 
 def three_delete():
     header_delete()
+    connection = sqlite3.connect("src/database/database.db")
+    cursor = connection.cursor()
+    delete = "DELETE FROM records"
+    cursor.execute(delete)
+    cursor.close()
+    connection.commit()
+    connection.close()
+    tqdmProgressBarDelete()
+
+    file = open('src/txt/records.txt', 'w')
+    file.truncate()
+    file.close()
 
     
 # < --------------- Funciones de menú principal--------------- >
--- src/cmd/utils.py
+++ src/cmd/utils.py
@@ -3,11 +3,15 @@ from progress.bar import *
 from tqdm import *
 import time
 
+spacer = "\n"
+spacer_main = "\n\n\n\n\n"
 # < --------------- Barras de progreso --------------- >
 
 def ChargingBarToClose():
+    system("cls")
+    print(spacer_main)
     text = ' '.center(45," ")
-    bar = FillingSquaresBar(text, max=100)
+    bar = PixelBar(text, max=100)
     for num in range(100):
         time.sleep(0.03)
         bar.next()
@@ -16,17 +20,27 @@ def ChargingBarToClose():
     exit(0)
 
 def ChargingBarToOpen():
+    system("cls")
+    print(spacer_main)
     text = ' '.center(45," ")
-    bar = PixelBar(text, max=100)
+    bar = FillingSquaresBar(text, max=100)
     for num in range(100):
         time.sleep(0.01)
         bar.next()
     bar.finish()
     system("cls")
 
-def tqdmProgressBar():
+def tqdmProgressBarSave():
     for item in tqdm(range(100)):
         time.sleep(0.01)
+
+def tqdmProgressBarDelete():
+    text = ' '.center(45," ")
+    bar = FillingCirclesBar(text, max=100)
+    for num in range(100):
+        time.sleep(0.03)
+        bar.next()
+    bar.finish()
 
 
 # < --------------- Titulos o encabezados --------------- >
@@ -40,7 +54,7 @@ def repeat(): #Función de código invalido| invalid code function
     
 def title_menu(): #Función de titulo | Title function
     system("cls")
-    print("\n\n\n\n\n")
+    print(spacer_main)
     print("╔══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╗".center(127," "))
     print("║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║".center(127," "))
     print("║  ╠══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╣  ║".center(127," "))
@@ -58,8 +72,7 @@ def title_menu(): #Función de titulo | Title function
     print("╚══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╝".center(127," "))
 
 def title_insert(): #Función titulo de insertar  | title_insert function
-    system("cls")
-    print("\n\n\n\n\n")
+    print(spacer)
     print("╔═══════════════════════════════╦════════╗".center(127," "))
     print("║            OPCIONES           ║ CÓDIGO ║".center(127," "))
     print("╠═══════════════════════════════╬════════╣".center(127," "))
@@ -73,15 +86,8 @@ def title_insert(): #Función titulo de insertar  | title_insert function
     print("║   REGRESAR AL MENÚ PRINCIPAL  ║    0   ║".center(127," "))
     print("╚═══════════════════════════════╩════════╝".center(127," "))
 
-def header_insert(): #Función encabezado de insertar| header_insert function
-    system("cls")
-    print("╔══════════════════════════════╗".center(127," "))
-    print("║        INSERTAR DATOS        ║".center(127," "))
-    print("╚══════════════════════════════╝".center(127," "))
-
 def title_delete(): #Función titulo de borrar  | title_delete function
-    system("cls")
-    print("\n\n\n\n\n")
+    print(spacer)
     print("╔═══════════════════════════════╦════════╗".center(127," "))
     print("║            OPCIONES           ║ CÓDIGO ║".center(127," "))
     print("╠═══════════════════════════════╬════════╣".center(127," "))
@@ -95,8 +101,20 @@ def title_delete(): #Función titulo de borrar  | title_delete function
     print("║   REGRESAR AL MENÚ PRINCIPAL  ║    0   ║".center(127," "))
     print("╚═══════════════════════════════╩════════╝".center(127," "))
 
+def header_insert(): #Función encabezado de insertar| header_insert function
+    system("cls")
+    print("╔══════════════════════════════╗".center(127," "))
+    print("║        INSERTAR DATOS        ║".center(127," "))
+    print("╚══════════════════════════════╝".center(127," "))
+
 def header_delete(): #Función encabezado de borrar| header_delete function
     system("cls")
     print("╔══════════════════════════════╗".center(127," "))
     print("║         BORRAR DATOS         ║".center(127," "))
+    print("╚══════════════════════════════╝".center(127," "))
+
+def header_show(): #Función encabezado de mostrar| header_show function
+    system("cls")
+    print("╔══════════════════════════════╗".center(127," "))
+    print("║         MOSTRAR DATOS        ║".center(127," "))
     print("╚══════════════════════════════╝".center(127," "))
