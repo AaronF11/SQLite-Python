808661a123b1fbe9fb8ce8f0d3a31b6af335f3a6
Aaron Flores Pasos <aaron.flores7908@alumnos.udg.mx>
2021-12-31 01:38:11 -0600

    Menú CMD insertar

--- src/cmd/functions.py
+++ src/cmd/functions.py
@@ -5,7 +5,7 @@ import time
 
 def main():
     while True:
-        title()
+        title_menu()
         op = int(input('>>> '))
         option(op)
 
@@ -15,9 +15,33 @@ def ChargingBarToClose():
     for num in range(100):
         time.sleep(0.03)
         bar.next()
-    bar.finish()    
+    bar.finish()
+    system("cls")    
     exit(0)
+
+def ChargingBarToOpen():
+    text = ' '.center(45," ")
+    bar = PixelBar(text, max=100)
+    for num in range(100):
+        time.sleep(0.03)
+        bar.next()
+    bar.finish()
+    system("cls")  
 
 def option(op):
     if op == 0:
         ChargingBarToClose()
+    
+    elif op == 1:
+        ChargingBarToOpen()
+        title_insert()
+        op_insert = int(input('>>> '))
+        
+    elif op == 2:
+        pass
+
+    elif op == 3:
+        pass
+
+    else:
+        pass
--- src/cmd/utils.py
+++ src/cmd/utils.py
@@ -1,6 +1,6 @@
 from os import system
 
-def title(): #Función de titulo | Title function
+def title_menu(): #Función de titulo | Title function
     system("cls")
     print("\n\n\n\n\n")
     print("╔══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╗".center(127," "))
@@ -18,3 +18,17 @@ def title(): #Función de titulo | Title function
     print("║  ╠══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╬══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╣  ║".center(127," "))
     print("║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║  ║".center(127," "))
     print("╚══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╝".center(127," "))
+
+def title_insert(): #Función titulo de insertar  | title_insert function
+    system("cls")
+    print("\n\n\n\n\n")
+    print("╔═══════════════════════════════╦════════╗".center(127," "))
+    print("║            OPCIONES           ║ CÓDIGO ║".center(127," "))
+    print("╠═══════════════════════════════╬════════╣".center(127," "))
+    print("║    INSERTAR UNICO REGISTRO    ║    1   ║".center(127," "))
+    print("╠═══════════════════════════════╬════════╣".center(127," "))
+    print("║     INSERTAR MÁS REGISTROS    ║    2   ║".center(127," "))
+    print("╠═══════════════════════════════╬════════╣".center(127," "))
+    print("║ INSERTAR REGISTROS Y GUARDAR  ║        ║".center(127," "))
+    print("║  EN UN ARCHIVO LOS REGISTROS  ║    3   ║".center(127," "))
+    print("╚═══════════════════════════════╩════════╝".center(127," "))
