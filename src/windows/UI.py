24d23d3296235f16c37b950e05b4ad5adb0f5c48
Aaron Flores Pasos <aaron.flores7908@alumnos.udg.mx>
2022-01-03 22:24:44 -0600

    Creando la ventana grafica

---
+++ src/windows/UI.py
@@ -0,0 +1,10 @@
+from tkinter import * 
+
+def main():
+
+    root = Tk()
+    root.title("BASE DE DATOS - RECORDS")
+    root.geometry("500x500")
+    root.iconbitmap("src/windows/ico/database.ico")
+
+    root.mainloop()
